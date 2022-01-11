1f5257c3e1ed402de1acfcc846e7e37f0cf9d16f
valentina <valentina.becerra@gmail.com>
2022-01-11 16:39:59 +0100

    version 0.0.1 preparation of the img_data  - go trouht directories  - extract files to check  - convert img into a nparray     - resize     - grayscale - save the dataset : array and file as label

---
+++ create_foto_database_array.py
@@ -0,0 +1,103 @@
+import os
+import imghdr
+import numpy as np
+import cv2
+from tkinter.filedialog import askdirectory
+
+"""
+version 0.0.1
+preparation of the img_data
+ - go trouht directories
+ - extract files to check
+ - convert img into a np.array
+    - resize 
+    - grayscale
+- save the dataset : array and file as label
+ 
+"""
+
+def select_imgs(src):
+    """
+    Input the source,
+    output: list of img_path to convert.
+    """
+    # if dir, extract the fotos
+    if os.path.isdir(src):
+        #l_path = list_img_path(src)
+        return list_img_path(src) #l_path # list files
+    #if source is only a file:
+    elif os.path.isfile(src) and imghdr.what(src) != 'gif':
+        return [src]
+    else: print ("directory or file not acepted, please select an other one!") #Error Value
+
+def list_img_path(directory):
+    """
+    input : directory
+    return: list of images from subdirectories
+    """
+    exts = ['jpeg', 'jpg', 'bmp', 'png']
+    list_img_path = list()
+    # go through the directory
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            # select a file
+            file_path = os.path.join(root, file)
+            # read the ext of file
+            img_ext = imghdr.what(file_path)
+            # if file is a img
+            if img_ext in exts:
+                # save to the img_path list
+                list_img_path.append(file_path)
+    return list_img_path
+
+
+def encode_img_list (l_pfad):
+    l_img_array = list()
+    for item in l_pfad:
+        #print("...1 decoding img :", item)
+        img_list = encode_img(item)
+        img_array = np.array(img_list, dtype=object)
+        if img_array.shape == (26 , 26):
+            l_img_array.append(img_array)
+
+        else:
+            print ("array size of {} is not ok".format(item))
+            l_img_array.append("array size is not ok")
+
+    return l_pfad, l_img_array
+
+def encode_img(img_file):
+    if imghdr.what(img_file):
+        img = cv2.imread(img_file, cv2.IMREAD_UNCHANGED)
+        # reduce
+        px_size = 26 #
+        resize_img = cv2.resize(img, dsize=(px_size, px_size), interpolation=cv2.INTER_CUBIC)
+        # convert in grey
+        gray_img = cv2.cvtColor(resize_img, cv2.COLOR_BGR2GRAY)
+        return gray_img
+    else:
+        print('file cannot be converted into a matrix')
+
+# helper functions
+def save_np(destination, data, label):
+    np.savez(destination, data=data, label=label)
+    pass
+
+def main():
+    #select dir or foto so convert into array
+    dir0 = askdirectory()
+    #choose the images, that can be converted into arrays
+    l_img_path_roh = select_imgs(dir0)
+    print('{} images were found.'.format(len(l_img_path_roh)))
+    #convert the imgs into arrays
+    l_img_path, l_img_array = encode_img_list(l_img_path_roh)
+    print('{} images were converted into an array.'.format(len(l_img_array)))
+    #save the database as .npz
+    destination = r'/output.npz'
+    save_np(destination, l_img_array, l_img_path)
+    pass
+
+main()
+
+
+
